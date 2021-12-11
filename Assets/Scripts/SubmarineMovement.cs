a7d146a04825118573eb990bf1df35545281e72f
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2021-12-11 14:25:28 +0000

    Add Flock Scripts

--- Assets/Scripts/SubmarineMovement.cs
+++ Assets/Scripts/SubmarineMovement.cs
@@ -21,12 +21,21 @@ public class SubmarineMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        if (x < 0)
+        float x = Mathf.Max(Mathf.Min(Input.GetAxis("Mouse X"), 1f), -1f);
+        float y = Mathf.Max(Mathf.Min(Input.GetAxis("Mouse Y"), 1f), -1f);
+
+        
+
+        if (_rigidbody.velocity.x < 0)
             _renderer.flipX = true;
         else
             _renderer.flipX = false;
-        _rigidbody.velocity = new Vector2(x * Speed * Time.deltaTime, y * Speed * Time.deltaTime);
+        //_rigidbody.velocity = new Vector2(x * Speed * Time.deltaTime, y * Speed * Time.deltaTime);
+
+        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        dir = dir.normalized * Speed * _rigidbody.mass;
+
+        _rigidbody.AddForce(dir);
     }
 }
