1e4a392994c8c8f443e8dffe4c0c4b75aafef419
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2021-12-11 13:04:47 +0000

    Added prefabs

--- Assets/Scripts/SubmarineMovement.cs
+++ Assets/Scripts/SubmarineMovement.cs
@@ -10,12 +10,13 @@ public class SubmarineMovement : MonoBehaviour
     public float Speed;
 
     private Camera theCam;
-    public SpriteRenderer Renderer;
+    private SpriteRenderer _renderer;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         theCam = Camera.main;
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -23,9 +24,9 @@ public class SubmarineMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         if (x < 0)
-            Renderer.flipX = true;
+            _renderer.flipX = true;
         else
-            Renderer.flipX = false;
+            _renderer.flipX = false;
         _rigidbody.velocity = new Vector2(x * Speed * Time.deltaTime, y * Speed * Time.deltaTime);
     }
 }
